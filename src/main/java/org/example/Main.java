3846cc3e05843e21154210e7150d0d8c3c294d07
jiahao ren <542251108@qq.com>
2023-06-12 08:54:48 +0800

    try to verify the effectiveness of RegisterUser sdk

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,39 @@
 package org.example;
 
+import org.example.register.model.RegisterUserResponse;
+import org.example.tutorial.UserServiceClient;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Replace these with your actual host and API key
+        String host = "http://localhost:8080";
+        String apiKey = "your-api-key";
+
+        // Create an instance of UserServiceClient
+        UserServiceClient userServiceClient = new UserServiceClient(host, apiKey);
+
+        try {
+            // Define your request parameters
+            String boxUUID = "your-box-uuid";
+            String userId = "your-user-id";
+            String subdomain = "your-subdomain";
+            String userType = "user_admin"; // or "user_member"
+            String clientUUID = "your-client-uuid";
+
+            // Call the registerUser method and get the response
+            String reqId = "e9993fc787d94b6c886cbaa340f9c0f4";
+            String boxRegKey = "brk_YVj29IJAD3";
+            RegisterUserResponse response = userServiceClient.registerUser(boxUUID, userId, subdomain, userType, clientUUID, reqId, boxRegKey);
+
+            // Now you can use the response object
+            System.out.println("Box UUID: " + response.getBoxUUID());
+            System.out.println("User ID: " + response.getUserId());
+            System.out.println("User Domain: " + response.getUserDomain());
+            System.out.println("User Type: " + response.getUserType());
+            System.out.println("Client UUID: " + response.getClientUUID());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/org/example/tutorial/UserServiceClient.java
+++ src/main/java/org/example/tutorial/UserServiceClient.java
@@ -21,7 +21,7 @@ public class UserServiceClient {
         this.apiKey = apiKey;
     }
 
-    public RegisterUserResponse registerUser(String boxUUID, String userId, String subdomain, String userType, String clientUUID) throws Exception {
+    public RegisterUserResponse registerUser(String boxUUID, String userId, String subdomain, String userType, String clientUUID, String reqId, String boxRegKey) throws Exception {
         RegisterUserRequest request = new RegisterUserRequest();
         request.setBoxUUID(boxUUID);
         request.setUserId(userId);
@@ -38,9 +38,16 @@ public class UserServiceClient {
                         .uri(URI.create(host + "/v2/platform/boxes/" + boxUUID + "/users"))
                         .header("Content-Type", "application/json")
                         .header("Api-Key", apiKey)
+                        .header("Request-Id", reqId) // Add this line
+                        .header("Box-Reg-Key", boxRegKey) // Add this line
                         .POST(HttpRequest.BodyPublishers.ofString(requestBody))
                         .build(),
                 HttpResponse.BodyHandlers.ofString());
+
+        // Handle error response from the server
+        if (httpResponse.statusCode() != 200) {
+            throw new Exception("Error response from the server: " + httpResponse.body());
+        }
 
         // Convert the HTTP response body from a JSON string to an UnmarshallerContext object
         UnmarshallerContext context = objectMapper.readValue(httpResponse.body(), UnmarshallerContext.class);
