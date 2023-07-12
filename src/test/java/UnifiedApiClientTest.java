44cd0324de039ea776e39ff8cd0b2fea02022ddc
jiahao ren <542251108@qq.com>
2023-07-13 00:23:12 +0800

    add test case for RegisterUser.

--- src/test/java/UnifiedApiClientTest.java
+++ src/test/java/UnifiedApiClientTest.java
@@ -1,6 +1,7 @@
 import org.example.Authentication.model.ObtainBoxRegKeyResponse;
 import org.example.register.model.RegisterClientResponse;
 import org.example.register.model.RegisterDeviceResponse;
+import org.example.register.model.RegisterUserResponse;
 import org.junit.Test;
 import org.junit.Assert;
 import org.example.Client.UnifiedApiClient;
@@ -81,6 +82,49 @@ public class UnifiedApiClientTest {
             // Here you could assert more response data if necessary.
         } catch (InterruptedException | ExecutionException e) {
             System.out.println("Error occurred in registering client: " + e.getMessage());
+        } finally {
+            client.shutdown();
+        }
+    }
+    @Test
+    public void testRegisterUser() {
+        // Create an instance of UnifiedApiClient
+        UnifiedApiClient client = new UnifiedApiClient("https://ao.space", "api-key", 10);
+        List<String> serviceIds = Arrays.asList("10001");
+
+        // Define required parameters
+        String userId = "1";
+        String subdomain = "";
+        String userType = "user_admin";
+        String clientUUID = "5d5af871790b4922bca935f08109a531";
+
+        // Call obtainBoxRegKey to get boxRegKey
+        Future<ObtainBoxRegKeyResponse> futureObtainBoxRegKeyResponse = client.obtainBoxRegKey(boxUUID, serviceIds, "sign", reqId);
+        String boxRegKey = "";
+        try {
+            ObtainBoxRegKeyResponse obtainBoxRegKeyResponse = futureObtainBoxRegKeyResponse.get();  // Blocks until the API response is available
+            for (ObtainBoxRegKeyResponse.TokenResult tokenResult : obtainBoxRegKeyResponse.getTokenResults()) {
+                boxRegKey = tokenResult.getBoxRegKey();  // Fetching the boxRegKey
+            }
+        } catch (InterruptedException | ExecutionException e) {
+            System.out.println("Error occurred in obtaining box reg key: " + e.getMessage());
+        }
+
+        // Ensure we have a boxRegKey before we proceed to register user
+        Assert.assertNotNull("boxRegKey should not be null", boxRegKey);
+
+        // Register the user with the obtained boxRegKey
+        Future<RegisterUserResponse> future = client.registerUser(boxUUID, userId, subdomain, userType, clientUUID, reqId, boxRegKey);
+        try {
+            RegisterUserResponse response = future.get();  // Blocks until the API response is available
+            System.out.println("Registered user with Box UUID: " + response.getBoxUUID());
+            System.out.println("Registered user with User ID: " + response.getUserId());
+            System.out.println("Registered user with User Domain: " + response.getUserDomain());
+            System.out.println("Registered user with User Type: " + response.getUserType());
+            System.out.println("Registered user with Client UUID: " + response.getClientUUID());
+            // Here you could assert more response data if necessary.
+        } catch (InterruptedException | ExecutionException e) {
+            System.out.println("Error occurred in registering user: " + e.getMessage());
         } finally {
             client.shutdown();
         }
